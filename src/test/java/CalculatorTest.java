92f625c17753986035b9a623b75187fe821d4c5e
Ekaterina <kaluginaec@mail.ru>
2026-03-22 23:11:23 +0300

    correction style_success

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -15,75 +15,75 @@ public class CalculatorTest {
     class InfixToPostfixTest {
         @Test
         void simple() {
-            assertThat(Calculator.infixtoPostfix(new ArrayList<>(List.of("a", "+", "b", "*", "c", "-", "d")))).containsExactly("a", "b", "c", "*", "+", "d", "-");
+            assertThat(Calculator.infixToPostfix(new ArrayList<>(List.of("a", "+", "b", "*", "c", "-", "d")))).containsExactly("a", "b", "c", "*", "+", "d", "-");
         }
 
         @Test
         void additionSubtraction() {
-            assertThat(Calculator.infixtoPostfix(new ArrayList<>(List.of("5", "+", "2", "-", "3")))).containsExactly("5", "2", "+", "3", "-");
+            assertThat(Calculator.infixToPostfix(new ArrayList<>(List.of("5", "+", "2", "-", "3")))).containsExactly("5", "2", "+", "3", "-");
         }
 
         @Test
         void multiplicationDivision() {
-            assertThat(Calculator.infixtoPostfix(new ArrayList<>(List.of("5", "*", "2", "/", "3")))).containsExactly("5", "2", "*", "3", "/");
+            assertThat(Calculator.infixToPostfix(new ArrayList<>(List.of("5", "*", "2", "/", "3")))).containsExactly("5", "2", "*", "3", "/");
         }
 
         @Test
         void power() {
-            assertThat(Calculator.infixtoPostfix(new ArrayList<>(List.of("k", "*", "2", "^", "3")))).containsExactly("k", "2", "3", "^", "*");
+            assertThat(Calculator.infixToPostfix(new ArrayList<>(List.of("k", "*", "2", "^", "3")))).containsExactly("k", "2", "3", "^", "*");
         }
 
         @Test
         void power2() {
-            assertThat(Calculator.infixtoPostfix(new ArrayList<>(List.of("3", "+", "k", "*", "2", "^", "3")))).containsExactly("3", "k", "2", "3", "^", "*", "+");
+            assertThat(Calculator.infixToPostfix(new ArrayList<>(List.of("3", "+", "k", "*", "2", "^", "3")))).containsExactly("3", "k", "2", "3", "^", "*", "+");
         }
 
         // ассоциативность возведения в степень: операцию выполняем справа налево
         @Test
         void twoPowers() {
-            assertThat(Calculator.infixtoPostfix(new ArrayList<>(List.of("k", "^", "2", "^", "3", "+", "5")))).containsExactly("k", "2", "3", "^", "^", "5", "+");
+            assertThat(Calculator.infixToPostfix(new ArrayList<>(List.of("k", "^", "2", "^", "3", "+", "5")))).containsExactly("k", "2", "3", "^", "^", "5", "+");
         }
 
 
         @Test
         void brackets() {
-            assertThat(Calculator.infixtoPostfix(new ArrayList<>(List.of("(", "a", "+", "b", ")", "*", "c", "-", "d")))).containsExactly("a", "b", "+", "c", "*", "d", "-");
+            assertThat(Calculator.infixToPostfix(new ArrayList<>(List.of("(", "a", "+", "b", ")", "*", "c", "-", "d")))).containsExactly("a", "b", "+", "c", "*", "d", "-");
         }
 
         @Test
         void twoBrackets() {
-            assertThat(Calculator.infixtoPostfix(new ArrayList<>(List.of("(", "a", "+", "b", ")", "*", "(", "c", "-", "d", ")")))).containsExactly("a", "b", "+", "c", "d", "-", "*");
+            assertThat(Calculator.infixToPostfix(new ArrayList<>(List.of("(", "a", "+", "b", ")", "*", "(", "c", "-", "d", ")")))).containsExactly("a", "b", "+", "c", "d", "-", "*");
         }
 
         @Test
         void twoOperationsInBrackets() {
-            assertThat(Calculator.infixtoPostfix(new ArrayList<>(List.of("k", "*", "(", "a", "*", "b", "+", "c", ")", "-", "3")))).containsExactly("k", "a", "b", "*", "c", "+", "*", "3", "-");
+            assertThat(Calculator.infixToPostfix(new ArrayList<>(List.of("k", "*", "(", "a", "*", "b", "+", "c", ")", "-", "3")))).containsExactly("k", "a", "b", "*", "c", "+", "*", "3", "-");
         }
 
         @Test
         void twoOperationsInBrackets2() {
-            assertThat(Calculator.infixtoPostfix(new ArrayList<>(List.of("k", "+", "(", "a", "+", "b", "*", "c", ")", "/", "3")))).containsExactly("k", "a", "b", "c", "*", "+", "3", "/", "+");
+            assertThat(Calculator.infixToPostfix(new ArrayList<>(List.of("k", "+", "(", "a", "+", "b", "*", "c", ")", "/", "3")))).containsExactly("k", "a", "b", "c", "*", "+", "3", "/", "+");
         }
 
 
         @Test
         void nestedBrackets() {
-            assertThat(Calculator.infixtoPostfix(new ArrayList<>(List.of("(", "(", "2", "-", "3", ")", ")")))).containsExactly("2", "3", "-");
+            assertThat(Calculator.infixToPostfix(new ArrayList<>(List.of("(", "(", "2", "-", "3", ")", ")")))).containsExactly("2", "3", "-");
         }
 
         @Test
         void singleElement() {
-            assertThat(Calculator.infixtoPostfix(new ArrayList<>(List.of("6")))).containsExactly("6");
+            assertThat(Calculator.infixToPostfix(new ArrayList<>(List.of("6")))).containsExactly("6");
         }
 
         @Test
         void missOpenBracketThrowsException() {
-            assertThatThrownBy(() -> Calculator.infixtoPostfix(new ArrayList<>(List.of("2", "+", "3", ")", "*", "5")))).isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> Calculator.infixToPostfix(new ArrayList<>(List.of("2", "+", "3", ")", "*", "5")))).isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
         void missCloseBracketThrowsException() {
-            assertThatThrownBy(() -> Calculator.infixtoPostfix(new ArrayList<>(List.of("(", "2", "+", "3", "*", "5")))).isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> Calculator.infixToPostfix(new ArrayList<>(List.of("(", "2", "+", "3", "*", "5")))).isInstanceOf(IllegalArgumentException.class);
         }
     }
 
@@ -91,85 +91,85 @@ public class CalculatorTest {
     class ResultByInfix {
         @Test
         void simple() {
-            assertThat(Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("3", "+", "4", "*", "2", "-", "7"))))).isEqualTo(4);
+            assertThat(Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("3", "+", "4", "*", "2", "-", "7"))))).isEqualTo(4);
         }
 
         @Test
         void additionSubtraction() {
-            assertThat(Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("5", "+", "2", "-", "3"))))).isEqualTo(4);
+            assertThat(Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("5", "+", "2", "-", "3"))))).isEqualTo(4);
         }
 
         @Test
         void multiplicationDivision() {
-            assertThat(Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("5", "*", "2", "/", "3"))))).isCloseTo(3.333, within(0.001));
+            assertThat(Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("5", "*", "2", "/", "3"))))).isCloseTo(3.333, within(0.001));
         }
 
         @Test
         void power() {
-            assertThat(Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("4", "*", "2", "^", "3"))))).isEqualTo(32);
+            assertThat(Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("4", "*", "2", "^", "3"))))).isEqualTo(32);
         }
 
         @Test
         void power2() {
-            assertThat(Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("3", "+", "6", "*", "2", "^", "3"))))).isEqualTo(51);
+            assertThat(Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("3", "+", "6", "*", "2", "^", "3"))))).isEqualTo(51);
         }
 
         // ассоциативность возведения в степень: операцию выполняем справа налево
         @Test
         void twoPowers() {
-            assertThat(Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("2", "^", "2", "^", "3", "+", "5"))))).isEqualTo(261);
+            assertThat(Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("2", "^", "2", "^", "3", "+", "5"))))).isEqualTo(261);
         }
 
 
         @Test
         void brackets() {
-            assertThat(Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("(", "3", "+", "4", ")", "*", "5", "-", "3"))))).isEqualTo(32);
+            assertThat(Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("(", "3", "+", "4", ")", "*", "5", "-", "3"))))).isEqualTo(32);
         }
 
         @Test
         void twoBrackets() {
-            assertThat(Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("(", "3", "+", "4", ")", "*", "(", "5", "-", "3", ")"))))).isEqualTo(14);
+            assertThat(Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("(", "3", "+", "4", ")", "*", "(", "5", "-", "3", ")"))))).isEqualTo(14);
         }
 
         @Test
         void twoOperationsInBrackets() {
-            assertThat(Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("2", "*", "(", "3", "*", "4", "+", "5", ")", "-", "3"))))).isEqualTo(31);
+            assertThat(Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("2", "*", "(", "3", "*", "4", "+", "5", ")", "-", "3"))))).isEqualTo(31);
         }
 
         @Test
         void twoOperationsInBrackets2() {
-            assertThat(Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("2", "+", "(", "3", "+", "4", "*", "5", ")", "/", "3"))))).isCloseTo(9.666, within(0.001));
+            assertThat(Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("2", "+", "(", "3", "+", "4", "*", "5", ")", "/", "3"))))).isCloseTo(9.666, within(0.001));
         }
 
 
         @Test
         void nestedBrackets() {
-            assertThat(Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("(", "(", "2", "-", "3", ")", ")"))))).isEqualTo(-1);
+            assertThat(Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("(", "(", "2", "-", "3", ")", ")"))))).isEqualTo(-1);
         }
 
         @Test
         void singleElement() {
-            assertThat(Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("6"))))).isEqualTo(6);
+            assertThat(Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("6"))))).isEqualTo(6);
         }
 
         @Test
         void missOpenBracketThrowsException() {
-            assertThatThrownBy(() -> Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("2", "+", "3", ")", "*", "5"))))).isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("2", "+", "3", ")", "*", "5"))))).isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
         void missCloseBracketThrowsException() {
-            assertThatThrownBy(() -> Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("(", "2", "+", "3", "*", "5"))))).isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("(", "2", "+", "3", "*", "5"))))).isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
         void notNumbersAndOperatorsThrowsException() {
-            assertThatThrownBy(() -> Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("(", "2", "+", "b", ")", "*", "5"))))).isInstanceOf(NumberFormatException.class);
+            assertThatThrownBy(() -> Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("(", "2", "+", "b", ")", "*", "5"))))).isInstanceOf(NumberFormatException.class);
         }
 
         @Test
         void UnknownOperatorThrowsException() {
-            assertThatThrownBy(() -> Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("5", "&", "3"))))).isInstanceOf(NumberFormatException.class);
+            assertThatThrownBy(() -> Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("5", "&", "3"))))).isInstanceOf(NumberFormatException.class);
         }
 
         @Test
@@ -184,7 +184,7 @@ public class CalculatorTest {
 
         @Test
         void divisionByZero() {
-            assertThatThrownBy(() -> Calculator.resultByPostfix(Calculator.infixtoPostfix(new ArrayList<>(List.of("5", "/", "0"))))).isInstanceOf(IllegalArgumentException.class);
+            assertThatThrownBy(() -> Calculator.resultByPostfix(Calculator.infixToPostfix(new ArrayList<>(List.of("5", "/", "0"))))).isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
